431c2b33c87660eff8d519416fa48a0a020563e3
Luke Baring <baringluke35@gmail.com>
2026-08-26 16:19:39 -0700

    Time: 0 ms (100%), Space: 8.7 MB (20.49%) - LeetHub

---
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long ans = 0;
+        while (x!=0){
+            ans = x % 10 + ans * 10;
+            x /= 10;
+        }
+        if ( ans < INT_MIN || ans > INT_MAX) {
+            return 0;
+        }
+        return ans;
+    }
+};
