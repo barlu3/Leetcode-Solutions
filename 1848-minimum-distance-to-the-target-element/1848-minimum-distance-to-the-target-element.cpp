d990ad7df4f187f0e263709bafa80e92959410ac
Luke Baring <baringluke35@gmail.com>
2026-09-08 17:51:15 -0700

    Time: 0 ms (100%), Space: 13.4 MB (75.1%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int getMinDistance(vector<int>& nums, int target, int start) {
-        int minDist = INT_MAX;
-        for (int i = 0; i < nums.size(); i++){
+        int minDist = INT_MAX, n = nums.size();
+        for (int i = 0; i < n; i++){
             if (nums[i] == target) {
                 minDist = std::min(minDist, std::abs(i - start));
             }
