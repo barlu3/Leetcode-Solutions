6cbda2d0d0d585c3ceaff76574ccc2e728368c78
Luke Baring <baringluke35@gmail.com>
2026-09-09 08:40:49 -0700

    8 ms (67.99%) 9.3 MB (77.48%) 0005-longest-palindromic-substring Medium

---
+++ LeetCode/Medium/0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int start = 0, maxLength = 1;
+
+        // Helper lambda to expand outward from a center
+        auto expand = [&](int left, int right) {
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                int currentLength = right - left + 1;
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                    start = left;
+                }
+                left--;
+                right++;
+            }
+        };
+
+        for (int i = 0; i < s.size(); ++i) {
+            expand(i, i);     // Odd length palindromes (e.g., "aba")
+            expand(i, i + 1); // Even length palindromes (e.g., "abba")
+        }
+
+        return s.substr(start, maxLength);
+    }
+};
