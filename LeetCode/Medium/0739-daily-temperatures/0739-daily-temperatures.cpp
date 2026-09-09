972e65e3b1c7fecd5edddb0836f152768edfe660
Luke Baring <baringluke35@gmail.com>
2026-09-09 08:21:19 -0700

    12 ms (92.07%) 107.3 MB (47.85%) 0739-daily-temperatures Medium

--- LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        size_t n = temperatures.size();
+        size_t n = temperatures.size(), prevIndex;
         vector<int> ans(n, 0);
         stack<int> stck;
 
         for (int i = 0; i < n; i++) {
             while (!stck.empty() && temperatures[i] > temperatures[stck.top()]) {
-                int prevIndex = stck.top();
+                prevIndex = stck.top();
                 stck.pop();
                 ans[prevIndex] = i - prevIndex;
             }
