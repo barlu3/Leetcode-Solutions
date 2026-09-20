1b7f10268e8d013bb10f39297680a634f0d4ec1e
Luke Baring <baringluke35@gmail.com>
2026-09-19 22:18:59 -0700

    0 ms (100%) 9.7 MB (78.4%) 3498-reverse-degree-of-a-string Easy

--- LeetCode/Easy/3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ LeetCode/Easy/3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        int ans = 0;
-        for (int i = 0; i < s.length(); ++i) {
+        int n = s.size(), ans = 0;
+        for (int i = 0; i < n; ++i) {
             ans += ((26 - (s[i] - 'a')) * (i + 1));
         }
         return ans;
